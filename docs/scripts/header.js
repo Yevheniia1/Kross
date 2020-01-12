265e856ce4e0b1809c4af0237040c582fbcc5c59
Yevheniia <y.potiichuk@gmail.com>
2020-01-12 11:56:27 +0200

    Добавлены скрипт и стили для декоративных элементов в header

--- docs/scripts/header.js
+++ docs/scripts/header.js
@@ -1,1 +1,18 @@
-{\rtf1}
+import {createNewElements} from './script.js';
+
+
+const containerForDecorations = document.querySelector('.header--home>.container>.decoration'),
+      containerAbout = document.querySelector('.about'),
+      classesOfDecorations = ['leaf-1', 'leaf-2', 'leaf-3', 'leaf-5', 'leaf-6', 'drops-1', 'drops-2'];
+
+//Создание декоративных элементов в header
+for(let value of classesOfDecorations){
+    let img = createNewElements(containerForDecorations, 'img', `${value}`)
+    img.setAttribute('src', `./imgs/decorations/${value}.png`)
+}
+
+//Создание декоративных элементов в about
+(() => {
+    let img = createNewElements(containerAbout, 'img', 'leaf-4');
+    img.setAttribute('src', `./imgs/decorations/leaf-4.png`)
+})()
---
+++ docs/scripts/script.js
@@ -0,0 +1,29 @@
+// Функция для создание детей(строка с тегом) с одинаковым классом в родителе(элемент)
+
+function createNewElements(parent, child, className) { 
+    
+    try {
+        function helperCreateAddElems(className) {
+            const elem = document.createElement(child);
+            elem.classList.add(className);
+            parent.append(elem);
+            return elem
+        }
+        
+        if(Array.isArray(className)){
+            let arr = [];
+            for(let value of className) {
+                
+                arr.push(helperCreateAddElems(value))
+            }
+        } else {
+            return helperCreateAddElems(className)
+        } 
+    } catch(err) {
+        throw err
+    }
+
+}
+
+
+export {createNewElements}
